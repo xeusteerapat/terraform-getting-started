9d098d28f1cd643491cf7e9041a614bfa1d49bdb
xeusteerapat <xeus085@gmail.com>
2022-08-13 15:56:04 +0700

    :boom: [Update] update access logs for alb and nginx index.html

--- 01-basics-nginx-webapp/instances.tf
+++ 01-basics-nginx-webapp/instances.tf
@@ -4,14 +4,18 @@ resource "aws_instance" "nginx1" {
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.nginx-sg.id]
-  tags                   = local.common_tags
+  iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
+  depends_on             = [aws_iam_role_policy.allow_s3_all]
+
+  tags = local.common_tags
 
   user_data = <<EOF
   #! /bin/bash
   sudo amazon-linux-extras install -y nginx1
   sudo service nginx start
+  aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/website/index.html /home/ec2-user/index.html
   sudo rm /usr/share/nginx/html/index.html
-  echo '<html><head><title>Xeus Team Server 1</title></head><body style="background-color: cyan"><h1>Hi, Terraform</h1></body></html>' | sudo tee /usr/share/nginx/html/index.html
+  sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html
   EOF
 }
 
@@ -20,6 +24,8 @@ resource "aws_instance" "nginx2" {
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.subnet2.id
   vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
+  depends_on             = [aws_iam_role_policy.allow_s3_all]
 
   tags = local.common_tags
 
@@ -27,7 +33,8 @@ resource "aws_instance" "nginx2" {
   #! /bin/bash
   sudo amazon-linux-extras install -y nginx1
   sudo service nginx start
+  aws s3 cp s3://${aws_s3_bucket.web_bucket.id}/website/index.html /home/ec2-user/index.html
   sudo rm /usr/share/nginx/html/index.html
-  echo '<html><head><title>Xeus Team Server 2</title></head><body style="background-color: cyan"><h1>Hi, Terraform</h1></body></html>' | sudo tee /usr/share/nginx/html/index.html
+  sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html
   EOF
 }
